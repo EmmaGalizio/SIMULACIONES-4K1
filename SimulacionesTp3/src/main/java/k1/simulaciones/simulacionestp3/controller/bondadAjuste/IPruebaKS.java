5cf5c60004c3c1a3f682c7290710becd065a69c2
Mateo Rey Nores <mateoreynores@hotmail.com>
2022-04-20 18:11:37 -0300

    interface ks

--- SimulacionesTp3/src/main/java/k1/simulaciones/simulacionestp3/controller/bondadAjuste/IPruebaKS.java
+++ SimulacionesTp3/src/main/java/k1/simulaciones/simulacionestp3/controller/bondadAjuste/IPruebaKS.java
@@ -14,22 +14,22 @@ public interface IPruebaKS {
 
     default List<IntervaloKS> calcularIntervalosKS(IntervaloKS[] intervalos){
 
-        float espAcumI = 0, acumJ = 0;
+        float espAcumI = 0;
         int li = 0, obsAcumI=0;
         List<IntervaloKS> intervalosKS = new ArrayList<>();
 
         for(int i = 0; i < intervalos.length; i++){
-            espAcumI += intervalos[i].getFrecEsp();
-            obsAcumI += intervalos[i].getFrecObs();
+            espAcumI += intervalos[i].getProbEsp();
+            obsAcumI += intervalos[i].getProbEsp();
             IntervaloKS intervalo = new IntervaloKS();
             intervalo.setLimInf(intervalos[li].getLimInf());
             intervalo.setLimSup(intervalos[i].getLimSup());
-            intervalo.setFrecEsp(espAcumI);
-            intervalo.setFrecObs(obsAcumI);
+            intervalo.setProbEsp(espAcumI);
+            intervalo.setProbObs(obsAcumI);
             intervalosKS.add(intervalo);
             espAcumI = 0;
             obsAcumI = 0;
-            acumJ = 0;
+
             li = i+1;
 
 
--- SimulacionesTp3/src/main/java/k1/simulaciones/simulacionestp3/controller/bondadAjuste/PruebaKS.java
+++ SimulacionesTp3/src/main/java/k1/simulaciones/simulacionestp3/controller/bondadAjuste/PruebaKS.java
@@ -1,4 +1,15 @@
-package k1.simulaciones.simulacionestp3.controller.bondadAjuste;
+import k1.simulaciones.simulacionestp3.controller.bondadAjuste.IPruebaKS;
+import k1.simulaciones.simulacionestp3.modelo.IntervaloKS;
+import k1.simulaciones.simulacionestp3.modelo.ParametrosCambioDistribucion;
+import k1.simulaciones.simulacionestp3.modelo.ResultadoBondadAjuste;
+import org.springframework.stereotype.Component;
 
-public class PruebaKS implements IPruebaKS{
+import java.util.List;
+
+public class PruebaKS implements IPruebaKS {
+    @Override
+    public ResultadoBondadAjuste generarPruebaKS(List<IntervaloKS> distFrecuencia, ParametrosCambioDistribucion parametros) {
+        return null;
+    }
 }
+
